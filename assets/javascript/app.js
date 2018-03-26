9fbd497d81023dd60c7a0a4c579de3adeb740bac
jmyager <jmyager6@gmail.com>
2018-03-25 21:37:07 -0400

    final update from 3-25-2018

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,7 @@ var correct = 0;
 var incorrect = 0;
 var userGuess;
 var rightWrong;
+var i = 0;
 
 // Define object containing all questions and answers
 var questionArray = [
@@ -21,37 +22,48 @@ var questionArray = [
         a3: "Answer 3",
         a4: "Answer 4",
         ca: "Answer 4"
+    },
+    {
+        q: "What is the flight trajectory of a seagull?",
+        a1: "Answer 1",
+        a2: "Answer 2",
+        a3: "Answer 3",
+        a4: "Answer 4",
+        ca: "Answer 4"
     }
 ];
 
 // Timer variables
 var count = 10;
 var intervalId;
+var questionIntervalId;
+
 
 // Define parent function to display questions, answers, and timers
-function loadQuestion(x) {
-    // Remove start button
-    $("#container").html("");
+function loadQuestion(questionSelect) {
+    // Remove start button and clear text
+    $("#container").empty();
+    $("#message-header").empty();
         
     // displayQuestion function is called to display first question & answers on the page.
-    displayQuestion(x);
+    displayQuestion(questionSelect);
 
     // Display timer, set intervalId and begin timer function
     $("#time").html(count);
     intervalId = setInterval(function(){questionTimer()},1000);
 
     // User pressed one of 4 answer buttons
-    checkAnswer(x);
+    checkAnswer(questionSelect);
 }
 
 // Define function that will clear previous question/answers and input new question and answers
 function displayQuestion(questionSelect) {
-    $("#question, #answer1, #answer2, #answer3, #answer4").html("");
-    $("#question").html(questionSelect.q);
-    $("#answer1").html(questionSelect.a1);
-    $("#answer2").html(questionSelect.a2);
-    $("#answer3").html(questionSelect.a3);
-    $("#answer4").html(questionSelect.a4);
+    $("#question, #answer1, #answer2, #answer3, #answer4").empty();
+    $("#question").html(questionSelect[i].q);
+    $("#answer1").html(questionSelect[i].a1);
+    $("#answer2").html(questionSelect[i].a2);
+    $("#answer3").html(questionSelect[i].a3);
+    $("#answer4").html(questionSelect[i].a4);
 }
 
 // Define function for 10 second timer that resets when it reaches 0.
@@ -72,58 +84,59 @@ function questionTimer() {
 function checkAnswer (questionSelect) {
     // User presses one of 4 answer buttons
     $("#answer1").click(function(){
-        userGuess = questionSelect.a1;
+        userGuess = questionSelect[i].a1;
         checkGuess(questionSelect);
     })
     $("#answer2").click(function(){
-        userGuess = questionSelect.a2;
+        userGuess = questionSelect[i].a2;
         checkGuess(questionSelect);
     })
     $("#answer3").click(function(){
-        userGuess = questionSelect.a3;
+        userGuess = questionSelect[i].a3;
         checkGuess(questionSelect);
     })
     $("#answer4").click(function(){
-        userGuess = questionSelect.a4;
+        userGuess = questionSelect[i].a4;
         checkGuess(questionSelect);
     })
 }
 
 // Define function to check if user's guess is correct
 function checkGuess (questionSelect) {
-    if (userGuess === questionSelect.ca){
+    if (userGuess === questionSelect[i].ca){
         correct++;
-        console.log("correct answers: " + correct);
-        rightWrong = true;
+        $("#message-header").html("You guessed correctly!");
     }
     else {
         incorrect++;
-        console.log("incorrect answers: " + incorrect);
-        rightWrong = false;
+        $("#message-header").html("You guess incorrectly!");
        
     }
     showCorrectAnswer();
+    clearInterval(intervalId);
 }
 
 // Define funcion to show correct answer content when user has answered or time has run out
 function showCorrectAnswer() {
-    $("#content").empty();
-    $("#content").html("The correct answer is...");
+    $("#question, #answer1, #answer2, #answer3, #answer4").empty();
+    $("#question").html("The correct answer is...");
     nextQuestion();
 }
 
 // define function to move to the next question in the array and check to see if it is the last question
 function nextQuestion() {
     if (i === questionArray.length) {
-        $("#content").empty();
-        $("#content").html("End of Triva Game!");
+        $("#question, #answer1, #answer2, #answer3, #answer4").empty();
+        $("#header-message").html("End of Triva Game!");
     }
     else {
-        questionIntervalId = setInterval(function(){loadQuestion(questionArray[i++])},5000);
+        i++;
+        questionIntervalId = setInterval(function(){loadQuestion(questionArray)},5000);
+        console.log(i);
     }
 }
 
 // User presses start button to start the game.
 $("#start").click(function() {
-    loadQuestion(questionArray[0]);
+    loadQuestion(questionArray);
 });
